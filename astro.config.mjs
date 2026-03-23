ff4d2d20144f8e8b029ae3aff576e543466685ad
Asís <38328023+soyasis@users.noreply.github.com>
2026-03-23 20:26:33 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-import pagefind from 'astro-pagefind';
+import { defineConfig } from "astro/config";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://getcai.app',
-  trailingSlash: 'always',
+  site: "https://getcai.app",
+  trailingSlash: "always",
   integrations: [pagefind()],
 });
